2aad3a4d7de7dd52d72f093f022d24ba2d49ebd3
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2017-01-11 18:56:10 -0200

    small changes to Dir var script

--- CODE/Dirichlet_variance.r
+++ CODE/Dirichlet_variance.r
@@ -1,10 +1,9 @@
 # Just a little something for us to begin studying how to do a PSA for \pi(\theta)
 # imagine an equal-parameter prior with X_i = x for a n-dimensional simplex
-
 dirichlet_var <- function(x, n){
   X <- rep(x, n)
   N <- n*x
-  return( (x*(N-x))/ (N^2*(N+1)))
+  return( (x*(N-x))/ (N^2*(N + 1)))
 }
 
 xs <- seq(.1, 10, .1)
@@ -14,7 +13,8 @@ dirichlet_var(1/2, 4)
 dirichlet_var(1/2, 8)
 
 svg("../plots/Dirichlet_var.svg")
-plot(xs, dirichlet_var(xs, 2), type = "l", lwd = 3, ylab = expression(Var(alpha[i])), xlab = expression(X[i]))
+plot(xs, dirichlet_var(xs, 2), type = "l", lwd = 3, 
+     ylab = expression(Var(alpha[i])), xlab = expression(X[i]))
 lines(xs, dirichlet_var(xs, 4), type = "l", col = 2, lwd = 3)
 lines(xs, dirichlet_var(xs, 5), type = "l", col = 3, lwd = 3)
 lines(xs, dirichlet_var(xs, 10), type = "l", col = 4, lwd = 3)
