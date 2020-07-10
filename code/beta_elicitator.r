6cbbce997a7af69de25ebb4101105a212c92831d
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2020-07-10 11:00:24 -0300

    minor fiddling

--- code/beta_elicitator.r
+++ code/beta_elicitator.r
@@ -21,7 +21,7 @@ elicit_beta_median_iq <- function(m, d, q = .90){
     b <- x[2]
     m.hat <- qbeta(.5, shape1 = a, shape2 = b)
     u.hat <- qbeta(q, shape1 = a, shape2 = b)
-    error <- .5 * (m.hat - m)^2 + .5*(u.hat-u)^2
+    error <-  (m.hat - m)^2 + (u.hat-u)^2
     return(error)  
   }
   opt <- suppressWarnings( optim(loss, par = c(1, 1), lower = c(1E-3, 1E-3)) )
@@ -48,7 +48,7 @@ elicit_beta_quantiles <- function(l, u, alpha = .95){
     l.hat <- qbeta(q0, shape1 = a, shape2 = b)
     u.hat <- qbeta(q1, shape1 = a, shape2 = b)
     # error <- (l.hat - l)^2 + (u.hat-u)^2
-    error <- abs(l.hat - l) + abs(u.hat-u) ## L1 norm is better
+    error <- abs(l.hat - l) + abs(u.hat-u) ## L1 norm: better?
     return(error)  
   }
   opt <- suppressWarnings( optim(loss, par = c(1, 1), lower = c(1E-3, 1E-3)) )
@@ -57,11 +57,11 @@ elicit_beta_quantiles <- function(l, u, alpha = .95){
   if(a < 0 || b < 0) warning("Warning: at least one of the obtained parameters is not valid")
   return(list(a = a, b = b))
 }
-### TEST
-# alpha <- .95
-# ll <- .8
-# uu <- .9
-# pars <- elicit_beta_quantiles(l = ll, u = uu)
+## TEST
+# alpha <- .99
+# ll <- .15
+# uu <- .2
+# pars <- elicit_beta_quantiles(l = ll, u = uu, alpha = alpha)
 # pars$a / (pars$a + pars$b) # mean
 # qbeta((1-alpha)/2, shape1 = pars$a, shape2 = pars$b)
 # qbeta(.5, shape1 = pars$a, shape2 = pars$b)
